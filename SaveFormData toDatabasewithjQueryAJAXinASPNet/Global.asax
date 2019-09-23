d4a4decb12bdc6ffd3b542ab56d9bc021c24271b
Ganesh <Ganesh@WINCTRL-UD7QMVI>
2019-09-23 14:20:41 -0400

    Add project files.

---
+++ SaveFormData toDatabasewithjQueryAJAXinASPNet/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SaveFormData_toDatabasewithjQueryAJAXinASPNet.Global" Language="C#" %>
---
+++ SaveFormData toDatabasewithjQueryAJAXinASPNet/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SaveFormData_toDatabasewithjQueryAJAXinASPNet")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("CtrlSoft")]
+[assembly: AssemblyProduct("SaveFormData_toDatabasewithjQueryAJAXinASPNet")]
+[assembly: AssemblyCopyright("Copyright © CtrlSoft 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("eceee2fc-0b24-4ebf-ab72-31b597227c1b")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
